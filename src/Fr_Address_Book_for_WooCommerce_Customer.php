93735cc45ca2d56f38c2da88ed3226d2e08e3243
Amos Lee <iwillhappy1314@gmail.com>
2021-09-01 17:46:28 +0800

    增加本地存储，完善收货地址UI

--- src/Fr_Address_Book_for_WooCommerce_Customer.php
+++ src/Fr_Address_Book_for_WooCommerce_Customer.php
@@ -9,17 +9,19 @@
 class Fr_Address_Book_for_WooCommerce_Customer {
     /**
      * Get addresses.
-     * 
+     *
      * @since 1.0.0
      * @return array
      */
     public function get_addresses() {
         $addresses = array();
-        
+
         foreach (wc()->customer->get_meta("fabfw_address", false) as $meta) {
             $addresses[$meta->id] = $meta->value;
         }
-        
+
+        // print_r($addresses);
+
         return $addresses;
     }
 }
